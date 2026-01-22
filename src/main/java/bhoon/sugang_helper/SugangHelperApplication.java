4afc4bd9be1d8a443f0d49a2baa2d0d7eaead04d
박병훈 <leafloveu@naver.com>
2026-01-22 19:53:12 +0900

    first commit

---
+++ src/main/java/bhoon/sugang_helper/SugangHelperApplication.java
@@ -0,0 +1,13 @@
+package bhoon.sugang_helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugangHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SugangHelperApplication.class, args);
+	}
+
+}
