84f43a120cc518c7bc86c6cd6278b6fe175b305e
박병훈 <leafloveu@naver.com>
2026-04-23 21:36:01 +0900

    feat(course): 단과대-학과 계층 구조 데이터 모델링 및 2026 학제 초기화 구현

---
+++ src/main/java/bhoon/sugang_helper/domain/course/repository/DepartmentRepository.java
@@ -0,0 +1,12 @@
+package bhoon.sugang_helper.domain.course.repository;
+
+import bhoon.sugang_helper.domain.course.entity.College;
+import bhoon.sugang_helper.domain.course.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    Optional<Department> findByNameAndCollege(String name, College college);
+    List<Department> findByCollegeId(Long collegeId);
+}
